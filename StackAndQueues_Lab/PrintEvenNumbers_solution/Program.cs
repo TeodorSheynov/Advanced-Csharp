90002785889940f3aeed61aaa72460be1ebffa53
Teodor <74595485+TeodorSheynov@users.noreply.github.com>
2021-01-04 16:50:28 +0200

    Queues

---
+++ StackAndQueues_Lab/PrintEvenNumbers_solution/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PrintEvenNumbers_solution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Queue<int> que = new Queue<int>(arr);
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (que.Peek()%2==0)
+                {
+                    que.Enqueue(que.Dequeue());
+                    
+                }
+                else
+                {
+                    que.Dequeue();
+                }
+            }
+            Console.Write(string.Join(", ",que));
+        }
+    }
+}
