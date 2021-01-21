6062699b250383e6051635e667fa9f2c121de9bb
Teodor <74595485+TeodorSheynov@users.noreply.github.com>
2021-01-21 17:19:23 +0200

    Add files via upload

---
+++ HashSets_Dictionaries/SetsOfElements_solution/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SetsOfElements_solution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] n = Console.ReadLine().Split((' '), StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            HashSet<int> setOne = new HashSet<int>(n[0]);
+            HashSet<int> setTwo = new HashSet<int>(n[1]);
+            for (int i = 0; i < n[0]; i++)
+            {
+                setOne.Add(int.Parse(Console.ReadLine()));
+            }
+            for (int i = 0; i < n[1]; i++)
+            {
+                setTwo.Add(int.Parse(Console.ReadLine()));
+            }
+            foreach (var item in setOne)
+            {
+                if (setTwo.Contains(item))
+                {
+                    Console.Write($"{item} ");
+                }
+            }
+        }
+    }
+}
---
+++ HashSets_Dictionaries/Wardrobe_solution/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Wardrobe_solution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int colorsCount = int.Parse(Console.ReadLine());
+            Dictionary<string, Dictionary<string, int>> wardrobe = new Dictionary<string, Dictionary<string, int>>();
+            for (int i = 0; i < colorsCount; i++)
+            {
+                string[] input = Console.ReadLine().Split(" -> ", StringSplitOptions.RemoveEmptyEntries);
+                string[] items = input[1].Split(",", StringSplitOptions.RemoveEmptyEntries);
+                string color = input[0];
+                if (!wardrobe.ContainsKey(color))
+                {
+                    wardrobe.Add(color, new Dictionary<string, int>());
+                }
+                for (int k = 0; k < items.Length; k++)
+                {
+                    if (wardrobe[color].ContainsKey(items[k]))
+                    {
+                        wardrobe[color][items[k]]++;
+                    }
+                    else
+                    {
+                        wardrobe[color].Add(items[k], 1);
+                    }
+
+                }
+            }
+            string[] search = Console.ReadLine().Split(' ',StringSplitOptions.RemoveEmptyEntries);
+            foreach (var colorR in wardrobe)
+            {
+                Console.WriteLine($"{colorR.Key} clothes:");
+                foreach (var cloth in colorR.Value)
+                {
+                    var clothitem = cloth.Key;
+                    var clothitemCount = cloth.Value;
+                    if (search[0]==colorR.Key && search[1]==cloth.Key)
+                    {
+                        Console.WriteLine($"* {clothitem} - {clothitemCount} (found!)");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"* {clothitem} - {clothitemCount}");
+                    }
+                }
+            }
+        }
+    }
+}
