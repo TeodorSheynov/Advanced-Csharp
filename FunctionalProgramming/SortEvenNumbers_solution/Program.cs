8d6f840919659103965194c1896b01afd71bd30d
Teodor <74595485+TeodorSheynov@users.noreply.github.com>
2021-01-28 23:41:55 +0200

    Add files via upload

---
+++ FunctionalProgramming/SortEvenNumbers_solution/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace SortEvenNumbers_solution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .OrderBy(x => x)
+                .Where(z => z % 2 == 0)
+                .ToArray();
+            Console.WriteLine(string.Join(' ',arr));
+        }
+    }
+}
---
+++ FunctionalProgramming/VAT_solution/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace VAT_solution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<double, double> vat = x => x = x + x * 0.2;
+            Func<string, double> parser = x => double.Parse(x);
+            double[] arr = Console.ReadLine()
+                 .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                 .Select(parser)
+                 .Select(vat)
+                 .ToArray();
+            foreach (var item in arr)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
