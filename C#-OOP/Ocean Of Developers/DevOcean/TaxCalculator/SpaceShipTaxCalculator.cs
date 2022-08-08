b4467f6b707f37db5b36327117b051c9373f51ae
TeodorSheynov <Teodor.Sheynov@gmail.com>
2022-08-08 15:02:29 +0300

    homework

---
+++ C#-OOP/Ocean Of Developers/DevOcean/TaxCalculator/SpaceShipTaxCalculator.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using TaxCalculator.Models;
+
+namespace TaxCalculator
+{
+    public class SpaceShipTaxCalculator:TaxCalculator
+    {
+        private readonly Spaceship _spaceship;
+        public SpaceShipTaxCalculator(Spaceship spaceship)
+        {
+            this._spaceship = spaceship;
+        }
+
+        protected override int TakeYearOfCalculation()
+        {
+            Console.WriteLine("Year of tax calculation: ");
+            var inputYear = Console.ReadLine();
+            if (!int.TryParse(inputYear, out var yearOfTaxCalculation))
+            {
+                throw new ArgumentException("Invalid type!");
+            }
+            if (yearOfTaxCalculation < _spaceship.YearOfPurchase)
+            {
+                throw new ArgumentException("It can calculate only taxes since the purchase of the vehicle :|");
+            }
+
+            return yearOfTaxCalculation;
+        }
+
+        public override decimal Calculate()
+        {
+            var yearOfCalculation = TakeYearOfCalculation();
+            var years = yearOfCalculation - _spaceship.YearOfPurchase;
+            var miles = _spaceship.MilesTraveled / 1000;
+            var initialTax = _spaceship.InitialTax;
+            var additionalTax = _spaceship.AdditionalTax;
+            var taxDecline = _spaceship.TaxDecline;
+
+
+            var taxes = initialTax + miles * additionalTax - years * taxDecline;
+            return taxes;
+        }
+    }
+}
